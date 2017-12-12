91f8f885da6db5ad31015f6a8ed010990841399d
Akaiju <john.buchaniec@loop.colum.edu>
2017-12-11 22:38:10 -0600

    Audio and menu button

--- GES-FA17-02-3D-Project-master/GES-FA17-02-3D-Project-master/Assets/Scripts/StartButton.cs
+++ GES-FA17-02-3D-Project-master/GES-FA17-02-3D-Project-master/Assets/Scripts/StartButton.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StartButton : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class StartButton : MonoBehaviour
+{
+    public void MenuStartButton()
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
 }
